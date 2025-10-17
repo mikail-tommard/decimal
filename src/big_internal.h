8289b15c92753fbefb4fae04842de617b05af300
mac <mac@MacBook-Pro-mac-2.local>
2025-10-17 09:56:07 -0400

    feat: заголовочный файл big_decimal

---
+++ src/big_internal.h
@@ -0,0 +1,22 @@
+#ifndef S21_BIG_INTERNAL_H
+#define S21_BIG_INTERNAL_H
+
+#include "include/s21_decimal.h"
+
+typedef struct {
+  uint32_t bits[7];
+  int scale;
+  int sign;
+} s21_big_decimal;
+
+void s21_big_set_zero(s21_big_decimal *d);
+int s21_big_is_zero(const s21_big_decimal d);
+
+int s21_big_from_decimal(s21_decimal src, s21_big_decimal *dst);
+int s21_big_to_decimal(s21_big_decimal src, s21_decimal *dst);
+
+int s21_big_mul10(s21_big_decimal *d);
+int s21_big_dib10(s21_big_decimal *d);
+int s21_big_scale_down_bankers(s21_big_decimal *d, int target_scale);
+
+#endif
